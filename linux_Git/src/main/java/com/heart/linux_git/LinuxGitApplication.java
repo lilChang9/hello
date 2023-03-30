7de94e35579606523f2e00791d2fbf5ad2325cba
heart <1044180889@qq.com>
2023-03-30 19:09:07 +0800

    test

---
+++ linux_Git/src/main/java/com/heart/linux_git/LinuxGitApplication.java
@@ -0,0 +1,13 @@
+package com.heart.linux_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxGitApplication.class, args);
+    }
+
+}
---
+++ linux_Git/src/test/java/com/heart/linux_git/LinuxGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heart.linux_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
